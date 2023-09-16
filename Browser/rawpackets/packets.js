106579c512e3067bcf0717b68c3d112e2b17ddc2
Ubuntu <ubuntu@aml-s905x-cc.lan>
2023-09-16 07:04:19 +0000

    Committer: Ubuntu <ubuntu@aml-s905x-cc.lan>  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   Browser/rawpackets/packets.css 	modified:   Browser/rawpackets/packets.js 	modified:   Browser/rawpackets/rawPackets.html 	modified:   Python/aprs.json

--- Browser/rawpackets/packets.js
+++ Browser/rawpackets/packets.js
@@ -1,6 +1,6 @@
 initGPS();
+let packets;
 async function initGPS(){
-    
     let url = window.location.href;
     if(url.includes("https://")){
         url = url.replace("https://", "");
@@ -20,6 +20,46 @@ async function initGPS(){
     })
     .then(res => res.json())
     .then(data => {
-        document.getElementById("packets").innerHTML = JSON.stringify(data,null,2);
+        packets = data;
+        createDivs(data);
     })
+    
+    document.getElementById("chosenCallsign").innerText = "CALLSIGN";
+    document.getElementById("packetDisplay").value = "";
 }
+
+function createDivs(d){
+    let callsigns = Object.keys(d);
+    for(let i = 0; i<Object.keys(d).length;i++){
+        let callsignDiv = document.createElement("div");
+        callsignDiv.setAttribute("class","CSbuttonDIV");
+
+        let selection = document.createElement("button");
+        selection.id=callsigns[i];
+        selection.setAttribute("class", "callsignButton")
+        selection.innerText = callsigns[i]
+        
+        callsignDiv.appendChild(selection)
+        document.getElementById("callsigns").appendChild(callsignDiv)
+    }
+    let buttons = document.querySelectorAll(".callsignButton");
+    for(let i = 0; i<buttons.length;i++){
+        buttons[i].addEventListener("click", showPackets);
+    }
+}
+
+function showPackets(e){
+    let cs = e.target.id;
+    let csPackets = packets[cs];
+
+    document.getElementById("chosenCallsign").innerText = cs;
+    document.getElementById("packetDisplay").value = JSON.stringify(csPackets,null,2);
+}
+
+document.getElementById("refreshData").addEventListener("click",function(){
+    let csButtonDivs = document.querySelectorAll(".CSbuttonDIV")
+    for(let i=0;i<csButtonDivs.length;i++){
+        csButtonDivs[i].remove()
+    }
+    initGPS();
+});
